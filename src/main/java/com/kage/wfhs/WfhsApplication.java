983711e5b796560e178561682b9f36392d1c768f
Vinnnm <thanthtoo1285@gmail.com>
2024-05-31 12:23:49 +0630

    Generate war file

--- src/main/java/com/kage/wfhs/WfhsApplication.java
+++ src/main/java/com/kage/wfhs/WfhsApplication.java
@@ -8,9 +8,16 @@ package com.kage.wfhs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WfhsApplication {
+public class WfhsApplication extends SpringBootServletInitializer {
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WfhsApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(WfhsApplication.class, args);
