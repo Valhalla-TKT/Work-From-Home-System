0db451a8dbe2ed6b6be7dc648690f4064ef426bd
25-00275 <25-00275@C01-DAT0131.dat.com>
2024-06-07 14:40:56 +0630

    css changes by kkl

--- src/main/resources/static/assets/form/applyForm.js
+++ src/main/resources/static/assets/form/applyForm.js
@@ -71,7 +71,7 @@ $(document).ready(function() {
 
     var regionSelect = $('#region-state-select');
     regionSelect.append('<option value="" disabled selected>Region/State</option>');
-    $.each(regions, function(index, region){		
+    $.each(regions, function(index, region){
         regionSelect.append('<option value="' + region.regionState + '">' + region.regionState + '</option>');
     });
 
@@ -345,12 +345,23 @@ proseMirrorEditor.addEventListener('input', function() {
         showNextDiv1();
     });
     
+	$('#go-back').click(function(event) {
+        showPreviousDiv();
+    });
+
+	$('#go-back-1').click(function(event) {
+        showPreviousDiv1();
+    });
 	function showNextDiv() {						
 		$("#data-section").hide();
 		$("#capture-section").show();
 		$("#signature-section").hide();
 		$("#go-back").show();
 		$("#go-back-1").hide();
+
+		$("#current-step-1").hide();
+		$("#current-step-2").show();
+		$("#current-step-3").hide();
 		if(checkBoxModalDialog.length) {
 			showCheckBoxDialogModal();	
 		}
@@ -363,6 +374,34 @@ proseMirrorEditor.addEventListener('input', function() {
 		$("#signature-section").show();
 		$("#go-back").hide();
 		$("#go-back-1").show();
+
+		$("#current-step-1").hide();
+		$("#current-step-2").hide();
+		$("#current-step-3").show();
+	}
+
+	function showPreviousDiv() {						
+		$("#data-section").show();
+		$("#capture-section").hide();
+		$("#signature-section").hide();
+		$("#go-back").hide();
+		$("#go-back-1").hide();
+
+		$("#current-step-1").show();
+		$("#current-step-2").hide();
+		$("#current-step-3").hide();
+	}
+
+	function showPreviousDiv1() {						
+		$("#data-section").hide();
+		$("#capture-section").show();
+		$("#signature-section").hide();
+		$("#go-back").show();
+		$("#go-back-1").hide();
+
+		$("#current-step-1").hide();
+		$("#current-step-2").show();
+		$("#current-step-3").hide();
 	}
 	
 	$("#checkbox-dialog-form").submit(function(event) {
